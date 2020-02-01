16e3924ac315dd96362c59222f2686501984a7a2
YueCHEN195 <740451313@qq.com>
2020-02-01 23:15:10 +0800

    2020-02-01

---
+++ admin/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './plugins/element.js'
+import router from './router'
+import http from './http'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$http = http    // 就可以 this.$http 来访问这个叫http的axios实例了
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
